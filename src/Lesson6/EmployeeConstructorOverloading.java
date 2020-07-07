a227bb84a3af994bf746cf860796802e6bb37e43
xaxage <xaxage@gmail.com>
2020-07-08 02:24:31 +0400

    Explanation update!!

--- src/Lesson6/EmployeeConstructorOverloading.java
+++ src/Lesson6/EmployeeConstructorOverloading.java
@@ -3,33 +3,38 @@ package Lesson6;
 public class EmployeeConstructorOverloading
     {
 
-    public EmployeeConstructorOverloading(int id2, String surename2, int age2)
+    public EmployeeConstructorOverloading(int id2, String surname2, int age2)
         {
-      this(surename2,id2);//it always has to be on the first line in  constructer. Otherwise it's an error -_-
+      this(surname2,id2);//it always has to be on the first line in  constructor.
+            // Otherwise it's an error -_-
+       //We can't use overloaded constructor in constructor , so we are using "this" ,
+            // instead of "EmployeeConstructorOverloading((surname2,id2))"
       id=id2;
         }
 
     protected EmployeeConstructorOverloading(String surename3,int age3)
         {
-        surename=surename3;
+        surname =surename3;
         age=age3;
         }
 
     EmployeeConstructorOverloading(int id4,String surename4,int age4,double salary4,String department4)
         {
-        this(surename4,age4);//it always has to be on the first line in  constructer. Otherwise it's an error -_-
+        this(surename4,age4);//it always has to be on the first line in  constructor.
+            // Otherwise it's an error -_-
+            //We can't use overloaded constructor in constructor , so we are using "this"
         id=id4;
-        departament=department4;
+        department =department4;
         salary=salary4;
         }
 
 
 
     int id;
-    String surename;
+    String surname;
     int age;
     double salary;
-    String departament;
+    String department;
     }
 class EmployeeTest
     {
@@ -38,8 +43,9 @@ class EmployeeTest
         EmployeeConstructorOverloading emp1 = new EmployeeConstructorOverloading(1,"Ivanov",25);
         System.out.println(emp1.id);
         EmployeeConstructorOverloading emp2=new EmployeeConstructorOverloading("Jackson",30);
-        System.out.println(emp2.surename);
+        System.out.println(emp2.surname);
         EmployeeConstructorOverloading emp3 = new EmployeeConstructorOverloading(2,"Berberyan",56,23.0,"MIT");
-        System.out.println(emp3.departament);
+        System.out.println(emp3.department);
+
         }
     }
